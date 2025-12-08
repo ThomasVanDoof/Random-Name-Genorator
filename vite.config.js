846f660fe7f10642f48019274578b7e095e3136e
Cave Smith <dracothedragon9292@gmail.com>
2025-12-08 16:37:10 -0700

    uh

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,8 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   root: "src",
-  base: "/Random-Name-Genorator/",
   build: {
-    outDir: "../dist",
+    outDir: "../dist",   // Put final build one folder above
     emptyOutDir: true
   }
 });
