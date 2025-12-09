60fcf895234714bc8e8f8fd5c2c33e8b39f01d1b
Cave Smith <dracothedragon9292@gmail.com>
2025-12-09 10:30:23 -0700

    try this

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,7 @@ document.getElementById("year").textContent = new Date().getFullYear();
 document.getElementById("last-modified").textContent = `Last Modified: ${document.lastModified}`;
 
 import { getRandomUser, getFantasyName } from "./ExternalServices.mjs";
-
+//not working seemingly, but it just need an update.
 const generateBtn = document.getElementById("generate-name-btn");
 const display = document.getElementById("random-name-display");
 
